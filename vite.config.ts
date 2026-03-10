d067584bb4297817a839f3a1c7bd2797a0728f3a
Mohammad Sadiq <sadikashrafi@Mohammads-MacBook-Air.local>
2026-03-11 00:55:49 +0530

    chore(deploy): switch deployment setup from GitHub Pages to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Visitor-Stream-Analytics/",
   plugins: [react()],
   resolve: {
     alias: {
